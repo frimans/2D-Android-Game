8e778aa9e53990da0de4493f2b7d88100537285b
Severi Friman <severi@pop-os.localdomain>
2021-08-29 22:20:22 +0300

    testing again

--- Platformer/Assets/LevelMenuScript.cs
+++ Platformer/Assets/LevelMenuScript.cs
@@ -8,6 +8,7 @@ public class LevelMenuScript : MonoBehaviour
     public TMPro.TMP_Text HighScoreText;
     public float highscore;
     // Start is called before the first frame update
+    // Test comment
     void Start()
     {
         highscore = PlayerPrefs.GetFloat("HighScore", 0);
